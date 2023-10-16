8812525dd9b872d1b2880b8f04b7ed05bfa2badb
Blake <blameswalker@gmail.com>
2023-10-16 13:40:45 -0700

    Implement #find_neighbors method for Knights; excludes visited positions and invalid positions

--- lib/knight.rb
+++ lib/knight.rb
@@ -2,14 +2,41 @@
 
 # Represents the knight
 class Knight
-  attr_reader :position, :visited
+  attr_reader :position, :visited, :neighbors
 
-  def initialize(position, visited = [])
+  TRANSFORMATIONS = [
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2],
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1]
+  ]
+
+  def initialize(position, visited = [position])
     @position = position
     @visited = visited
+
+    @neighbors = find_neighbors
   end
 
   def distance_traveled
     @visited.length
+  end
+
+  private
+
+  def find_neighbors
+    array = []
+    TRANSFORMATIONS.each do |trans|
+      array << [trans[0] + @position[0], trans[1] + @position[1]]
+    end
+
+    array.keep_if do |neighbor|
+      !@visited.include?(neighbor) &&
+      neighbor.all? { |int| int.between?(0, 7) }
+    end
   end
 end

b5858e55b83d00c20c3ea0b73691340f9200fe7e
Blake <blameswalker@gmail.com>
2023-10-16 13:39:35 -0700

    Require start and target coordinates to initialize a Board; implement color-coded #print method

--- lib/board.rb
+++ lib/board.rb
@@ -1,32 +1,55 @@
 # frozen_string_literal: true
 
+require 'colorize'
+
 # Represents the chess board
 class Board
-  attr_reader :coordinates
+  attr_reader :matrix
 
-  def initialize
-    @coordinates = generate_coordinates
+  def initialize(start, target)
+    @matrix = generate_matrix
+
+    @start = start
+    @target = target
+  end
+
+  def print
+    @matrix.each do |position|
+      str = position.to_s
+
+      if position == @start
+        str = str.red
+      elsif position == @target
+        str = str.yellow
+      end
+
+      if position[0] == 7
+        super("#{str}\n")
+      else
+        super("#{str} ")
+      end
+    end
   end
 
   private
 
-  def generate_coordinates
+  def generate_matrix
     x = 0
     y = 0
-    coordinates = []
+    matrix = []
 
     64.times do
       if x == 7
-        coordinates << [x, y]
+        matrix << [x, y]
 
         x, y = 0, y + 1
       else
-        coordinates << [x, y]
+        matrix << [x, y]
 
         x += 1
       end
     end
 
-    coordinates
+    matrix
   end
 end

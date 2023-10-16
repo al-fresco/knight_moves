1d9c2598831fa1dddd7e184899d5d933b6aa3716
Blake <blameswalker@gmail.com>
2023-10-16 14:01:05 -0700

    Rework Board matrix generation as a class method; since the matrix will never change, it is generated once and stored in a constant variable

--- lib/board.rb
+++ lib/board.rb
@@ -4,10 +4,32 @@ require 'colorize'
 
 # Represents the chess board
 class Board
+  MATRIX = Board.generate_matrix
+
   attr_reader :matrix
 
+  def self.generate_matrix
+    x = 0
+    y = 0
+    matrix = []
+
+    64.times do
+      if x == 7
+        matrix << [x, y]
+
+        x, y = 0, y + 1
+      else
+        matrix << [x, y]
+
+        x += 1
+      end
+    end
+
+    matrix
+  end
+
   def initialize(start, target)
-    @matrix = generate_matrix
+    @matrix = MATRIX
 
     @start = start
     @target = target
@@ -31,25 +53,9 @@ class Board
     end
   end
 
-  private
-
-  def generate_matrix
-    x = 0
-    y = 0
-    matrix = []
-
-    64.times do
-      if x == 7
-        matrix << [x, y]
-
-        x, y = 0, y + 1
-      else
-        matrix << [x, y]
-
-        x += 1
-      end
-    end
-
-    matrix
+  def knight_moves()
+    queue = []
+    
+    knight = Knight.new(@start)
   end
 end

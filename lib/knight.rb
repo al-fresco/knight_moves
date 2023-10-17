b9e5e8ff1b9e51405cb0858e9652c75832c07542
Blake <blameswalker@gmail.com>
2023-10-16 23:59:05 -0700

    Add #add_viable_path instance method and ::shortest_path class method to the Knight class

--- lib/knight.rb
+++ lib/knight.rb
@@ -6,16 +6,24 @@ class Knight
 
   @@viable_paths = []
 
+  def self.shortest_path
+    @@viable_paths.min_by { |path| path.length }.first
+  end
+
   def initialize(start)
     @start = start
     @path = [start]
   end
 
   def depth
-    @path.length
+    @path.length - 1
   end
 
   def move_to(position)
     @path << position
+  end
+
+  def add_viable_path
+    @@viable_paths << @path
   end
 end

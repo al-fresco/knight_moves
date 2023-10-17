41aa12ff73f8c2ab61ac0b90937e75593d4ff875
Blake <blameswalker@gmail.com>
2023-10-17 15:07:27 -0700

    Add @queue attribute to each Knight object

--- lib/knight.rb
+++ lib/knight.rb
@@ -2,10 +2,12 @@
 
 # Represents the knight; new knights are initialized with their parent's path
 class Knight
+  attr_accessor :queue
   attr_reader :path
 
   def initialize(parent_path = [])
     @path = parent_path
+    @queue = []
   end
 
   def visit(position)
